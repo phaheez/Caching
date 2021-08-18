5083db7a3cf4a51dd411186b78f1ca4b349792a3
Faiz Fasasi <faizfasasi@gmail.com>
2021-08-18 18:12:21 +0100

    Add project files.

---
+++ CachingSample/Controllers/TestController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using CachingSample.Services;
+using Microsoft.Extensions.Caching.Memory;
+using Microsoft.Extensions.Logging;
+
+namespace CachingSample.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly ILogger<TestController> _logger;
+        private readonly IMemoryCache _memoryCache;
+
+        public TestController(ILogger<TestController> logger, IMemoryCache memoryCache)
+        {
+            _logger = logger;
+            _memoryCache = memoryCache;
+        }
+
+        [HttpGet("cache/get/countries")]
+        public async Task<IActionResult> GetCountries()
+        {
+            const string cacheKey = "ListOfCountries";
+
+            if (!_memoryCache.TryGetValue(cacheKey, out List<Country> countries))
+            {
+                countries = await CountriesService.GetAllCountries();
+
+                var cacheEntryOptions = new MemoryCacheEntryOptions
+                {
+                    AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5), //expire in 5 minutes
+                    Priority = CacheItemPriority.High,
+                    SlidingExpiration = TimeSpan.FromDays(2), //evict if not accessed for 2 days
+                    Size = 1024 //size 1MB
+                };
+
+                _memoryCache.Set(cacheKey, countries, cacheEntryOptions);
+            }
+
+            return Ok(countries);
+        }
+
+        [HttpPost("cache/add")]
+        public async Task<IActionResult> AddCache()
+        {
+            var countries = await CountriesService.GetAllCountries();
+
+            var cacheExpiryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(5),
+                Priority = CacheItemPriority.High,
+                SlidingExpiration = TimeSpan.FromMinutes(2),
+                Size = 1024,
+            };
+            _memoryCache.Set("ListOfCountries", countries, cacheExpiryOptions);
+            _memoryCache.TryGetValue("ListOfCountries", out List<Country> values);
+
+            return Ok(values);
+        }
+
+        [HttpGet("cache/get")]
+        public IActionResult GetCache()
+        {
+            _memoryCache.TryGetValue("ListOfCountries", out List<Country> values);
+
+            return Ok(values);
+        }
+        
+        [HttpPost("cache/remove")]
+        public IActionResult RemoveCache()
+        {
+            _memoryCache.Remove("ListOfCountries");
+            _memoryCache.TryGetValue("ListOfCountries", out List<Country> values);
+
+            return Ok(values);
+        }
+    }
+}
---
+++ CachingSample/Services/CountriesService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace CachingSample.Services
+{
+    public static class CountriesService
+    {
+        public static async Task<List<Country>> GetAllCountries()
+        {
+            var file = await System.IO.File.ReadAllTextAsync("countries.json");
+            var countries = JsonConvert.DeserializeObject<List<Country>>(file);
+
+            return countries;
+        }
+    }
+}
